519b65681c8eb6ace30fc9357ba1aca7fdc7d195
Myokyeong Kim <77683645+data04190@users.noreply.github.com>
2022-04-12 00:25:39 +0900

    KRX 종목상태 데이터 수집 코드 (초기 1회만 실행)

---
+++ crawling/first_fullcode.py
@@ -0,0 +1,29 @@
+import pickle
+import json
+import requests
+import pandas as pd
+from io import BytesIO
+import pytz
+from datetime import datetime, date, timedelta
+
+
+headers = {'User-Agent': 'Chrome/93.0.4577.82 Safari/537.36,'}
+
+data = {'mktsel': 'ALL',
+         'typeNo': '0',
+         'searchText': '',
+         'bld': 'dbms/comm/finder/finder_stkisu',}
+
+url = "http://data.krx.co.kr/comm/bldAttendant/getJsonData.cmd"
+
+r = requests.post(url,data,headers=headers)
+j = json.loads(r.text)
+
+df = pd.json_normalize(j['block1'])
+df = df[['full_code','short_code','codeName','marketEngName']]
+
+df['status'] = '상장'
+
+
+df.to_csv('./fullcode.csv', index = False)
+:
